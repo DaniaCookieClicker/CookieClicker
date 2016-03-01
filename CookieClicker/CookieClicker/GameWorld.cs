e1defb4ad43e9934233a30167c0d3f77950af86c
NielsJustesen <niels449@hotmail.com>
2016-03-01 10:23:17 +0100

    Combat og GetGold.

--- CookieClicker/CookieClicker/GameWorld.cs
+++ CookieClicker/CookieClicker/GameWorld.cs
@@ -4,13 +4,22 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Drawing;
+using System.Threading;
 
 namespace CookieClicker
 {
+        
+    
     class GameWorld
     {
+        private static bool one = true;
+        private static bool two = false;
+        private static bool three = false;
+        private static bool four = false;
+        private static bool five = false;
+        private static bool six = false;
         private static int bossHealth = 6;
-        private static int playerDmg = 1;
+        private static int playerDmg = 10;
         private static int gold = 0;
         private static List<GameObject> toRemove = new List<GameObject>();
         public static List<GameObject> toAdd = new List<GameObject>();
@@ -21,7 +30,6 @@ namespace CookieClicker
         private BufferedGraphics backBuffer;
         private Rectangle displayRectangle;
         private Rectangle displayRect;
-
         public static List<GameObject> GameObj
         {
             get { return gameObj; }
@@ -107,6 +115,8 @@ namespace CookieClicker
                     GameObj.Add(go);
                 }
             }
+            Combat();
+            GetGold();
             toAdd.Clear();
             UpdateAnimations(currentFps);
             Update(currentFps);
@@ -142,10 +152,96 @@ namespace CookieClicker
 
             Font f = new Font("Arial", 16);
             dc.DrawString(string.Format("Boss Health: {0}", bossHealth), f, Brushes.Black, 200, 200);
+            dc.DrawString(string.Format("Gold: {0}", gold), f, Brushes.Black, 200, 216);
 #if DEBUG
             dc.DrawString(string.Format("FPS: {0}", currentFps), f, Brushes.Black, 100, 100);
 #endif 
             backBuffer.Render();
+        }
+        private static void Combat()
+        {
+            if (BossHealth <= 0 && one)
+            {
+                BossHealth = 12;
+                two = true;
+                one = false;
+            }
+            if (BossHealth <= 0 && two)
+            {
+                BossHealth = 24;
+                three = true;
+                two = false;
+            }
+            if (BossHealth <= 0 && three)
+            {
+                BossHealth = 48;
+                four = true;
+                three = false;
+            }
+            if (BossHealth <= 0 && four)
+            {
+                BossHealth = 48 * 2;
+                five = true;
+                four = false;
+            }
+            if (BossHealth <= 0 && five)
+            {
+                BossHealth = 48 * 4;
+                six = true;
+                five = false;
+            }
+        }
+        private static void GetGold()
+        {
+            int level = 1;
+            if (one == true)
+            {
+                if (level == 1)
+                {
+                    gold += bossHealth;
+                    level = 2;
+                }
+            }
+            if (two == true)
+            {
+                if (level == 2)
+                {
+                    gold += bossHealth;
+                    level = 3;
+                }
+            }
+            if (three == true)
+            {
+                if (level == 3)
+                {
+                    gold += bossHealth;
+                    level = 4;
+                }
+            }
+            if (four == true)
+            {
+                if (level == 4)
+                {
+                    gold += bossHealth;
+                    level = 5;
+                }
+            }
+            if (five == true)
+            {
+                if (level == 5)
+                {
+                    gold += bossHealth;
+                    level = 6;
+                }
+            }
+            if (six == true)
+            {
+                if (level == 6)
+                {
+                    gold += bossHealth;
+                    level = 7;
+                }
+            }
         }
     }
 }
